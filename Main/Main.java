591cab018b855ec7c30843ff54d233bcf570db59
zackcglee <leechanggong@gmail.com>
2022-02-03 11:35:42 +0900

    sixth

--- Main/Main.java
+++ Main/Main.java
@@ -2,7 +2,6 @@ package Main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         String isHellow = "Hello";
         if(isHellow.equals("Hello")){
             System.out.println("Hello");
