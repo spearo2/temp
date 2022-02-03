8ef12983c64fe8e8207cee08a461a8b67b8b076b
zackcglee <leechanggong@gmail.com>
2022-02-03 11:22:54 +0900

    second

--- Main/Main.java
+++ Main/Main.java
@@ -1,15 +1,15 @@
 package Main;
 
 public class Main {
-    // public static void main(String[] args) {
-    //     System.out.println("Hello World!");
-    //     String isHellow = "Hello";
-    //     if(isHellow.equals("Hello")){
-    //         System.out.println("Hello");
-    //     }
-    //     else{
-    //         System.out.println("Not Hello");
-    //     }
-    // }
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String isHellow = "Hello";
+        if(isHellow.equals("Hello")){
+            System.out.println("Hello");
+        }
+        else{
+            System.out.println("Not Hello");
+        }
+    }
 }
 
